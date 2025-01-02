80270635f876292fd2ca02fc24e14fdbb7030f5b
nadeemsousaf <nmsou6754@gmail.com>
2025-01-02 17:00:27 -0500

    basic_layout_temp

--- main_game.py
+++ main_game.py
@@ -1,1 +1,14 @@
 import pygame
+from sys import exit
+
+pygame.init()
+window = pygame.display.set_mode((800,400))
+pygame.display.set_caption('throwaway_name')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
