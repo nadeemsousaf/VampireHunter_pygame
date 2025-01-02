63892b75aaf2f9bc9faf59260506cd5e4519dece
nadeemsousaf <nmsou6754@gmail.com>
2025-01-02 17:23:22 -0500

    basic_bg

--- main_game.py
+++ main_game.py
@@ -4,6 +4,13 @@ from sys import exit
 pygame.init()
 window = pygame.display.set_mode((800,400))
 pygame.display.set_caption('throwaway_name')
+clock = pygame.time.Clock()
+
+#for images: Image = pygame.image.load('pathname/')
+sky = pygame.Surface((800,280))
+sky.fill('blue')
+grass = pygame.Surface((800,120))
+grass.fill('green')
 
 while True:
     for event in pygame.event.get():
@@ -11,4 +18,7 @@ while True:
             pygame.quit()
             exit()
 
+    window.blit(sky, (0,0))
+    window.blit(grass, (0,280))
     pygame.display.update()
+    clock.tick(60)
