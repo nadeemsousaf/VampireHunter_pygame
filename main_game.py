2aa105d086c55234cdc5780f9c3fcbf0e05613e5
nadeemsousaf <nmsou6754@gmail.com>
2025-05-08 09:03:47 -0400

    health

--- main_game.py
+++ main_game.py
@@ -27,19 +27,36 @@ class Player():
     def attack(self):
         possible_enemy = any_overlap(player,enemy_list)
         if possible_enemy != False:
-            possible_enemy.vanquish()
+            possible_enemy.damage()
+    def damage(self):
+        self.health -= 10
+        player_hb.remove_health()
+        if (self.health <= 0):
+            self.vanquish()
+    def vanquish(self):
+        print("placeholder")
 
 class Enemy():
-    def __init__(self, x, y, image):
+    def __init__(self, x, y, normal, hurt):
         self.x = x
         self.y = y
-        self.image = image
+        self.show = normal
+        self.normal = normal
+        self.hurt = hurt
         self.health = 100
-    def vanquish(self):
-        self.x += 30
+    def attack(self):
+        if (ob_overlap(player,self)):
+            player.damage()
+    def damage(self):
+        self.show = self.hurt
+        self.x += 45
         self.health -= 25
         if (self.health <= 0):
-            print("vanquished")
+            self.vanquish()
+        #pygame.time.delay(1000)
+        #self.show = self.normal
+    def vanquish(self):
+        print("placeholder")
 
 class Button():
     def __init__(self, x, y, image):
@@ -50,15 +67,30 @@ class Button():
         window.blit(self.image, (self.x, self.y))
         pos = pygame.mouse.get_pos
 
-enemy_1 = Enemy(0, 240, pygame.image.load('images/enemy1.png'))
+class HealthBar():
+    def __init__(self,name,x,y,width,height):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.height = height
+        self.red_width = width
+        self.green_width = width
+    def draw(self):
+        pygame.draw.rect(window, (255,0,0), pygame.Rect(self.x,self.y,self.red_width,self.height))
+        pygame.draw.rect(window, (0,128,0), pygame.Rect(self.x,self.y,self.green_width,self.height))
+    def remove_health(self):
+        self.green_width -= 10
+
+enemy_1 = Enemy(0, 240, pygame.image.load('images/enemy1.png'), pygame.image.load('images/enemy2.png'))
 player = Player(pygame.image.load('images/player1.png'), pygame.image.load('images/player3.png'), pygame.image.load('images/player2.png'))
 enemy_list = [enemy_1]
+player_hb = HealthBar("player",30,30,100,20)
 
 def ob_overlap(ob1, ob2):
     ob1_x1 = ob1.x
     ob1_x2 = ob1.x + ob1.show.get_width()
     ob2_x1 = ob2.x
-    ob2_x2 = ob2.x + ob2.image.get_width()
+    ob2_x2 = ob2.x + ob2.show.get_width()
     if (ob1_x1 <= ob2_x1 and ob1_x2 >= ob2_x1) or (ob2_x1 <= ob1_x1 and ob2_x2 >= ob1_x2):
         return True
     else:
@@ -69,7 +101,7 @@ def any_overlap(person, list):
     person_x2 = person.x + person.show.get_width()
     for x in list:
         enemy_x1 = x.x
-        enemy_x2 = x.x + x.image.get_width()
+        enemy_x2 = x.x + x.show.get_width()
         if (person_x1 <= enemy_x1 and person_x2 >= enemy_x1) or (enemy_x1 <= person_x1 and enemy_x2 >= person_x2):
             return x
     return False
@@ -107,8 +139,9 @@ while True:
     window.blit(grass, (0,70))
     window.blit(text_surface, (200,70))
     #for x in enemy_list:
-    window.blit(enemy_1.image, (enemy_1.x,enemy_1.y))
+    window.blit(enemy_1.show, (enemy_1.x,enemy_1.y))
     window.blit(player.show, (player.x,player.y))
+    player_hb.draw()
 
     player.x += player.speed*player.x_dir
     enemy_1.x -= 4
@@ -123,6 +156,10 @@ while True:
     
     if player.x > 800:
         player.x = 800-player.show.get_width()
+
+    for x in enemy_list:
+        if (ob_overlap(player,x)==False):
+            x.show = x.normal
 
     pygame.display.update()
     clock.tick(40)
